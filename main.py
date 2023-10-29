311e4449b9a50e470ce5c7cd462abe12f1d29cfa
Yegor-own <egor891712@gmail.com>
2023-10-29 19:46:15 +0500

    feat: all

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import telegram.error
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, PicklePersistence
+import env
 
 from yt_dlp import YoutubeDL
 import os
 import uuid
 
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Привет я Джокербот помогу скачать любое видео из Vk, YouTube или Dzen.\nПросто кидай мне сылку, а я попробую скачать.")
+    await update.message.reply_text(
+        "Привет я Джокербот помогу скачать любое видео из Vk, YouTube или Dzen.\nПросто кидай мне сылку, а я попробую скачать.")
+
 
 async def load(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-
     id = str(uuid.uuid4())
     URL = update.message.text
-    opts = {'outtmpl': './' + id + '.%(ext)s'}
-    path = id + ".mp4"
+    opts = {'outtmpl': './' + id+".mp4"}
+    path = id+".mp4"
+
+    await update.message.reply_text('Скачиваю это может занять время')
+
     err_code = 0
     with YoutubeDL(opts) as ydl:
         err_code = ydl.download(URL)
@@ -24,14 +28,18 @@ async def load(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if err_code:
         await update.message.reply_text('Упс что-то пошло не так')
     else:
-        try:
-            await update.message.reply_video(video=open(path, 'rb'))
-        except telegram.error.NetworkError:
-            await update.message.reply_text('Похоже видео слшком большое')
+
+        await update.message.reply_video(video=open(path, 'rb'))
         os.remove(path)
 
 
-app = ApplicationBuilder().token("6583238890:AAGh4IfVBy1RB3J5-fE4h4SHUVr7rklgrRU").build()
+
+my_persistence = PicklePersistence(filepath='per')
+
+app = (ApplicationBuilder().token(env.BOT_TOKEN) \
+       .local_mode(local_mode=True) \
+       .base_url("http://localhost:8081/bot") \
+       .build())
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, load))
